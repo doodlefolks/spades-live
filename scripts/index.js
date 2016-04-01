df3a6f1ed2c0e8bc41e64f59c2d0e2df24d78b95
Andrew Lowe <doodlefolks@gmail.com>
2016-04-01 11:11:43 -0700

    Fix #6

--- scripts/index.js
+++ scripts/index.js
@@ -6,9 +6,22 @@
       if (!data.val()) return;
       const games = data.val();
       const gameKeys = Object.keys(games);
+      const gameList = $('#game-list');
 
+      gameList.html('');
       for (let i = 0; i < gameKeys.length; i++) {
-        // Fill in the game-list div
+        const gameDiv = $('\
+          <div>\
+            <h1>Game ' + games[gameKeys[i]].gameNumber + '</h1>\
+            <p>Players</p>\
+            <ol></ol>\
+          </div>');
+        const players = games[gameKeys[i]].players;
+        for (let j = 0; j < Object.keys(players).length; j++) {
+          const playerLi = $('<li>' + players[j] + '</li>');
+          gameDiv.find('ol').append(playerLi);
+        }
+        gameList.append(gameDiv);
       }
     });
 
